a64be3438068e45c04642491d4c2fc1d492109cd
William Diaz <80552314+Williamddg@users.noreply.github.com>
2025-11-07 21:41:52 -0500

    interfaz main y nuva_condicion

--- app/src/main/java/com/example/motorinferencia/Nueva_condicion.java
+++ app/src/main/java/com/example/motorinferencia/Nueva_condicion.java
@@ -9,26 +9,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Nueva_condicion extends AppCompatActivity {
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_nueva_condicion);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        // spinner_regla traemos reglas y elejimos
+        // spinner_y_o traemos si y u o
         // edit text txt_atributo tomamos atributo - variable
         // spinner_igualdad traemos equivalencia ( = < > )
         // edit txt txt_valor tomamos valor
-        // button btn_agregar_regla agrega regla y muestra un toast
-        // textview menu1 refresca activity
-        // textview menu2 lanza acitvty nueva_condicion
+        // button btn_agregar_condicion agrega condicion a regla elejida y muestra un toast
+        // textview menu1 lanza actvity mainactivity
+        // textview menu2 refresca activity
         // textview menu3 lanza acitvty resultados
         // textview menu4 lanza acitvty grafo
     }
